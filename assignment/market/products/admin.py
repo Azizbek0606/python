8b27eaf50a1ffc76b6c68c83337cebf68ae5d235
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2025-01-08 10:51:33 +0500

    update

--- assignment/market/products/admin.py
+++ assignment/market/products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Logo
 
-# Register your models here.
+class Display_logo(admin.ModelAdmin):
+    list_display = ('logo', )
+
+    def logo_tag(self, obj):
+        if obj.logo:
+            return format_html('<img src="{}" style="height: 50px;"/>', obj.logo.url)
+        return "No Logo"
+    logo_tag.short_description = "Logo"
+
+admin.site.register(Logo, Display_logo)
--- assignment/market/products/models.py
+++ assignment/market/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Logo(models.Model):
+    logo = models.ImageField(upload_to='branding/')
+
+    def __str__(self):
+        return "Main Logo"
+    class Meta:
+        verbose_name_plural = "Logos"
+        verbose_name = "Logo"
