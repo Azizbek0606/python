e36514faad4e77610c0b543405cddea5b3574fb7
Azizbek Xabibullayev <xabibullayeva676@gmail.com>
2025-12-11 12:14:31 +0500

    update

---
+++ adventage programming/ds/tasks.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+a8 = Node('8')
+a7= Node('7')
+a6= Node('6')
+a5= Node('5',None, a8)
+a4= Node('4')
+a3= Node('3',a6,a7)
+a2= Node('2',a4,a5)
+a1= Node('1', a2,a3)
+
+
+def find_biggest(root):
+    if root is None:
+        return '0'
+
+    current = root.value
+    left = find_biggest(root.left)
+    right = find_biggest(root.right)
+
+    return max(current, left, right)
+
+
+def deepest_path(root):
+    if root is None:
+        return [], 0
+
+    left_path, left_depth = deepest_path(root.left)
+    right_path, right_depth = deepest_path(root.right)
+
+    if left_depth > right_depth:
+        return [root.value] + left_path, left_depth + 1
+    else:
+        return [root.value] + right_path, right_depth + 1
+
+
+print(deepest_path(a1))
