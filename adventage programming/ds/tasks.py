37e4b80fed1a2677b58db474eb45087fcbe0097e
Azizbek Xabibullayev <xabibullayeva676@gmail.com>
2025-12-16 16:49:15 +0500

    update

--- adventage programming/ds/tasks.py
+++ adventage programming/ds/tasks.py
@@ -14,17 +14,17 @@ a2= Node('2',a4,a5)
 a1= Node('1', a2,a3)
 
 
-def find_biggest(root):
-    if root is None:
-        return '0'
-
-    current = root.value
-    left = find_biggest(root.left)
-    right = find_biggest(root.right)
-
-    return max(current, left, right)
-
-
+# def find_biggest(root):
+#     if root is None:
+#         return '0'
+#
+#     current = root.value
+#     left = find_biggest(root.left)
+#     right = find_biggest(root.right)
+#
+#     return max(current, left, right)
+#
+#
 def deepest_path(root):
     if root is None:
         return [], 0
@@ -38,4 +38,42 @@ def deepest_path(root):
         return [root.value] + right_path, right_depth + 1
 
 
-print(deepest_path(a1))
+# print(deepest_path(a1))
+
+
+def BT_min(root):
+    if root is None:
+        return None
+
+    left = BT_min(root.left)
+    right = BT_min(root.right)
+    current = root.value
+    if left is not None:
+        current = min(left, current)
+    if right is not None:
+        current = min(right, current)
+    return current
+
+# print(BT_min(a1))
+import copy
+
+def deepest_path_tree(root, lst):
+    if root is None:
+        return lst
+
+    lst.append(root.value)
+    left_lst = copy.deepcopy(lst)
+    right_lst = copy.deepcopy(lst)
+
+    left_path = deepest_path_tree(root.left, left_lst)
+    right_path = deepest_path_tree(root.right, right_lst)
+
+    if len(left_path) > len(right_path):
+        return left_path
+    return right_path
+
+a = deepest_path_tree(a1,[])
+b = " -> ".join(a)
+print(b)
+
+
